c22c26bf7460b36ea7278fd7af82ae6766309054
Vaibhav Kumar <130498822+vaibhavkr87@users.noreply.github.com>
2024-12-16 16:30:03 +0530

    error resolve

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import HomePage from "./Home/HomePage";
 import AboutPage from "./About/AboutPage";
-import ContactPage from "./Contact/contactPage";
-import ProjectsPage from "./Projects/projectsPage";
-import ServicePage from "./Service/servicePage";
+import ContactPage from "./Contact/ContactPage";
+import ProjectsPage from "./Projects/ProjectsPage";
+import ServicePage from "./Service/ServicePage";
 
 export {HomePage, AboutPage, ContactPage, ProjectsPage, ServicePage}
